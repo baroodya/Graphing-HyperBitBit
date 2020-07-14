821c535713d3020adb967db9d1b8936f39f43d62
baroodya <abaroody@princeton.edu>
2020-07-14 17:35:37 -0400

    Cleaned up TimingTracker.java

--- src/main/java/TimingTracker.java
+++ src/main/java/TimingTracker.java
@@ -10,16 +10,20 @@ import java.time.Duration;
 import java.util.Scanner;
 
 public class TimingTracker {
+    // Static method to add a time to the file
     public static String add(String alg, String fileName, int substreams, int t, double time) throws IOException {
+        // Initialize the scanner
         File file = new File("src/timings.txt");
         Scanner sc = new Scanner(file);
 
-        String id = alg + " " + fileName + " " + substreams + " " + t;
-        String line = "";
+        // Create an id for the experiment
+        String id = alg + fileName + substreams + "|" + t;
+        String line = null;
 
+        // Go through timings.txt and determine whether this experiment has been run before
         boolean seen = false;
         StringBuilder sb = new StringBuilder();
-        String next = "";
+        String next;
         while (sc.hasNextLine()) {
             next = sc.nextLine();
             sb.append(next);
@@ -30,6 +34,7 @@ public class TimingTracker {
         }
         sc.close();
 
+        // if it has, update the average in the file
         if (seen) {
             String[] parts = line.split(" ");
             double avg = Double.parseDouble(parts[2]);
@@ -40,32 +45,36 @@ public class TimingTracker {
             String replace = id + " " + avg + " " + m;
 
             FileWriter writer = new FileWriter("src/timings.txt");
-            writer.write(sb.toString().replace("\n" + line, replace));
+            writer.write(sb.toString().replace(line, "\n" + replace));
             writer.close();
-        } else {
-            line = id + " " + time + " " + 1;
+        } // If it hasn't add it to the file
+        else {
+            line = id + " " + time + " " + "1";
             FileWriter writer = new FileWriter("src/timings.txt", true);
             writer.write("\n" + line);
             writer.close();
         }
 
+        // Return the duration as a three part time (HH:MM:SS)
         Duration duration = Duration.ofSeconds((long) Math.ceil(time));
         return duration.toHoursPart() + " hours, " + duration.toMinutesPart() + " minutes, and " + duration.toSecondsPart() + " seconds.";
     }
 
+    // Static Method to fetch a timing from the file
     public static String timing(String alg, String fileName, int m, int t) throws FileNotFoundException {
+        // Initialize the scanner
         File file = new File("src/timings.txt");
         Scanner sc = new Scanner(file);
 
-        String id = alg + " " + fileName + " " + m + " " + t;
+        // Create an id for the experiment
+        String id = alg + fileName + m + "|" + t;
         String line = "";
 
+        // Search for the id in the file
         boolean seen = false;
-        StringBuilder sb = new StringBuilder();
-        String next = "";
+        String next;
         while (sc.hasNextLine()) {
             next = sc.nextLine();
-            sb.append(next);
             if (next.contains(id)) {
                 line = next;
                 seen = true;
@@ -74,12 +83,14 @@ public class TimingTracker {
         }
         sc.close();
 
+        // if you find it, return the appropriate estimate for timing
         if (seen) {
             String[] parts = line.split(" ");
             double avg = Double.parseDouble(parts[2]);
             Duration duration = Duration.ofSeconds((long) Math.ceil(avg));
             return "This experiment will take about " + duration.toHoursPart() + " hours, " + duration.toMinutesPart() + " minutes, and " + duration.toSecondsPart() + " seconds.";
-        } else
+        } // if you don't, return an error statement
+        else
             return "This specific experiment has not been run before. I'm too lazy to give you an estimate.";
     }
 
