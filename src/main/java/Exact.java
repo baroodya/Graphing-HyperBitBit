cbdc856187db1883738b58bb78f8bd2ffde3ffad
baroodya <abaroody@princeton.edu>
2020-07-14 15:06:32 -0400

    Cleaned up Exact.java

--- src/main/java/Exact.java
+++ src/main/java/Exact.java
@@ -7,21 +7,24 @@ import java.util.Arrays;
 import java.util.HashSet;
 
 public class Exact {
+    // Returns the exact cardinality of stream
     public static int count(StringStream stream) {
-        HashSet<String> hset = new HashSet<String>();
+        HashSet<String> hset = new HashSet<>();
         for (String x : stream) hset.add(x);
         return hset.size();
     }
 
+    // Returns the exact cardinality of the first maxRead lines of file fileName
     public static int count(String fileName, int maxRead) throws FileNotFoundException {
         StringStream stream = new StringStream(fileName, maxRead);
         return count(stream);
     }
 
+    // Returns the cardinality after every iteration of stream
     public static int[] countArray(StringStream stream) throws FileNotFoundException {
         int[] counts = new int[total(stream)];
         stream.resetStream();
-        HashSet<String> hset = new HashSet<String>();
+        HashSet<String> hset = new HashSet<>();
         int i = 0;
         for (String x : stream) {
             hset.add(x);
@@ -31,17 +34,20 @@ public class Exact {
         return counts;
     }
 
+    // Returns the cardinality after each line if fileName, up to maxRead lines
     public static int[] countArray(String fileName, int maxRead) throws FileNotFoundException {
         StringStream stream = new StringStream(fileName, maxRead);
         return countArray(stream);
     }
 
+    // Returns the total number of Strings in stream
     public static int total(StringStream stream) {
         int N = 0;
-        for (String x : stream) N++;
+        for (String ignored : stream) N++;
         return N;
     }
 
+    // Returns the total number of lines in fileName, up to maxRead
     public static int total(String fileName, int maxRead) throws FileNotFoundException {
         StringStream stream = new StringStream(fileName, maxRead);
         return total(stream);
@@ -49,7 +55,7 @@ public class Exact {
 
     public static void main(String[] args) throws FileNotFoundException {
         String fileName = "src/datasets/f0";
-        int N = 1000000; //Integer.parseInt(args[1]);
+        int N = 1000000;
         StringStream stream = new StringStream(fileName, N);
         long total = total(stream);
         stream.resetStream();
