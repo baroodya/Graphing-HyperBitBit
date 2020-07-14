a8a3536810924c34711cb98e306b1463b2d6375c
baroodya <abaroody@princeton.edu>
2020-07-14 17:14:24 -0400

    Cleaned up Separator.java

--- src/main/java/Separator.java
+++ src/main/java/Separator.java
@@ -8,10 +8,13 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Separator {
+    // Static method to take a file fileName and seperate it into lines based on the regular expression regex
     public static void separate(String fileName, String regex) throws IOException {
+        // initialize the file and scanner
         File file = new File(fileName);
         Scanner scanner = new Scanner(file);
 
+        // Build a string that is what we want the file to look like
         String[] words;
         StringBuilder sb = new StringBuilder();
         while (scanner.hasNextLine()) {
@@ -20,6 +23,7 @@ public class Separator {
         }
         scanner.close();
 
+        // Write the string to the file, over writing what was already there
         FileWriter writer = new FileWriter(fileName);
         writer.write(sb.toString());
         writer.close();
